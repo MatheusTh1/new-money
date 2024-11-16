5eda96d8a0c38793854d62e42232382faff1690b
Matheus <matheushenriquesilva.th@gmail.com>
2024-11-15 21:12:25 -0300

    feat: add data-table and update font

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,13 +1,23 @@
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionsColumns } from "./_columns";
 
 const TransactionsPage = async () => {
   // acessar as transações do meu banco de dados
   const transactions = await db.transaction.findMany({});
   return (
-    <div>
-      {transactions.map((transactions) => (
-        <div key={transactions.id}>{transactions.name}</div>
-      ))}
+    <div className=" p-6 space-y-6">
+      {/* {TITULO E BOTAO} */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUpIcon></ArrowDownUpIcon>
+        </Button>
+      </div>
+      <DataTable columns={transactionsColumns} data={transactions} />
     </div>
   );
 };
